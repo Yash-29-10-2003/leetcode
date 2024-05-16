9165157ea875bf6a91254c109b4f9f9facc6b44c
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2024-05-16 13:30:55 +0530

    Time: 2 ms (97.61%) | Memory: 44.7 MB (54.42%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,5 +1,8 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //we are just creating a hash map  where we store the value of parsed integer from the
+        //list to check them later with other elements by seeing if 
+        //target - futureELE = pastELE
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i =   0 ;  i < nums.length ; i++){
             int diff;
