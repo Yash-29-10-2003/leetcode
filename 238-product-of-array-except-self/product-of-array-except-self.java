4599569f7b63646751afe082e5f64101c14c12c8
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2024-05-17 17:57:30 +0530

    Time: 3 ms (26.24%) | Memory: 55 MB (71.74%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,8 +1,12 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        //we can think of the resultant array as for each array[i]
+        //array[i] = Mult of right of Array[i] * Mult of left of Array[i]
+
         int[] resArray = new int[nums.length];
         Arrays.fill(resArray, 1);
 
+        //multiplying every ele of the result array with ele's on its left side
         int leftMult = 1;
         for (int i = 0 ; i < nums.length ; i++){
             if (i == 0){
@@ -13,6 +17,7 @@ class Solution {
             }
         }
 
+        //multiplying every ele of the result array with ele's on its right side
         int rightMult = 1;
         for (int i = nums.length -1 ; i > -1 ; i--){
             if (i == nums.length -1){
