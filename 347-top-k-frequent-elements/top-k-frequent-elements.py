1ba116b1b58ebe51b5b6589ba5d91241cf7e1f39
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2024-12-16 22:27:57 +0530

    Time: 49 ms (5.01%) | Memory: 20.3 MB (54.42%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         mapA = {}
+        #populate the map as integer : number of occurances for every k : v
         for i in nums:
             if i in mapA:
                 mapA[i] += 1
@@ -8,12 +9,16 @@ class Solution:
                 mapA[i] = 1
         
         res = []
+
+        #make a new list for occurances by getting the values from the dict and sort them
         occurances = mapA.values()
         occurances = sorted(occurances)
+
+        #get the key for k elements using the k values from the end of the sorted list
         for j in range(k):
             key = next((k for k, v in mapA.items() if v == occurances[len(occurances) - j - 1]), None)
             res.append(key)
-            del mapA[key]
+            del mapA[key]            #delete k:v pairs after going thru them once
         
         return res
         
