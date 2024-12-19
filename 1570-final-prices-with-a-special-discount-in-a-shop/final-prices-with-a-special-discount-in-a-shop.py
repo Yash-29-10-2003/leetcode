75695a7874cf86104668b96d456c178ed6893cf0
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2024-12-19 10:21:28 +0530

    Time: 3 ms (54.81%) | Memory: 18 MB (10.80%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,5 +1,6 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
+        # used double loops here , look for stack / monotonous stack implementation when done
         for i in range(len(prices)):
             if i == len(prices) -1 :
                 break
