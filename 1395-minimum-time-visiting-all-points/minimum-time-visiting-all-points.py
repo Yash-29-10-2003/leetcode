47f04b2b7d363373450f2e722ea616f695490336
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2024-12-19 22:41:42 +0530

    Time: 3 ms (39.87%) | Memory: 17.8 MB (11.54%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -4,6 +4,6 @@ class Solution:
         x1 , y1 = points.pop()     #we start from the last element because its the same thing both ways
         while points != []:        #couldve done while points:
             x2,y2 = points.pop()
-            time += max(abs(x1-x2) , abs(y1-y2))
+            time += max(abs(x1-x2) , abs(y1-y2))     #for each point to be reached the time is going to be the maximum difference between each coordinate points
             x1,y1 = x2,y2
         return time
