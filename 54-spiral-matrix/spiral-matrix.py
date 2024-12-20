a272e3d2aef412e674079a83a8ba0f31c57ac13e
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2024-12-20 13:51:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.89%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,26 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        #for left , right , top and bottom we do not store them as direct coordinates but only the leftmost side of the matrix or the top most side of the matrix
+        #also , bottom and right pointers are padded by 1 for ease
+        left, top = 0 , 0       
+        bottom, right = len(matrix) , len(matrix[0])
         res = []
-        left, right = 0, len(matrix[0])
-        top, bottom = 0 , len(matrix)
-        
+
         while left < right and top < bottom:
-            #getting every i for top row 
+            #adding all elements for the top row and right column
             for i in range(left,right):
                 res.append(matrix[top][i])
             top += 1
-            #every i for right row
             for i in range(top,bottom):
                 res.append(matrix[i][right-1])
             right -= 1
-            #checking if the matrix pointers are still alright
+
+            #we need to check mid loop for singular row inputs if we have completed them
             if not(left < right and top < bottom):
                 break
-            #every i for bottom row
-            for i in range(right-1, left-1 , -1):
+
+            #now for the bottom row and left column
+            for i in range(right-1,left-1, -1):
                 res.append(matrix[bottom-1][i])
             bottom -= 1
-            for i in range(bottom -1 , top-1 , -1):
+            for i in range(bottom-1,top-1, -1):
                 res.append(matrix[i][left])
             left += 1
+        
         return res
+        
