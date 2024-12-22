f850f84eac7c75437a763fb769a5974bd825d04a
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2024-12-22 14:05:01 +0530

    Time: 19 ms (10.07%) | Memory: 19.3 MB (9.07%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -8,13 +8,16 @@ class Solution:
             if abs(nums[p1]) < abs(nums[p2]):
                 sqr[curr] = nums[p2] ** 2
                 p2 -= 1
+                curr -= 1
             elif abs(nums[p1]) > abs(nums[p2]):
                 sqr[curr] = nums[p1] ** 2
                 p1 += 1
-            else:
+                curr -= 1
+            if abs(nums[p1]) == abs(nums[p2]):
                 sqr[curr] = nums[p2] ** 2
                 p2 -= 1
-            curr -= 1
+                curr -= 1
+        
         return sqr
 
         
