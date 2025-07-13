aa82390354ab44f4ab9a2f894ee61fc00861a667
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2025-07-13 21:51:11 +0530

    Time: 2 ms (98.98%) | Memory: 45.1 MB (46.92%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //we are just creating a hash map  where we store the value of parsed integer from the
-        //list to check them later with other elements by seeing if 
-        //target - futureELE = pastELE
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i =   0 ;  i < nums.length ; i++){
-            int diff;
-            diff =  target - nums[i];
-            if (hashMap.containsKey(diff)){
-                return new int[]{i,hashMap.get(diff)};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length ; i++){
+            int diff = target - nums[i];
+            if (map.containsKey(diff)){
+                return new int[] { i, map.get(diff) };
             }
-            hashMap.put(nums[i],i);
+            else{
+                map.put(nums[i], i);
+            }
         }
-        return new int[]{0,1};
+        return new int[] {22,221};
     }
 }
