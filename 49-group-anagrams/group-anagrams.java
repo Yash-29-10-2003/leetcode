f620a9bffe695719f2626c2a9979f55c22c11478
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2025-08-02 21:30:24 +0530

    Time: 6 ms (98.47%) | Memory: 48 MB (43.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,22 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //we have a map where we store sorted strings as keys and add the relevant strings
-        //as values to it , then in the end we just return  out all the values as
-        //a list of list
-        HashMap<String, List<String>> hashMap = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            char[] currArray = strs[i].toCharArray();
-            Arrays.sort(currArray);
-            String currSorted = new String(currArray);
-            
-            if (hashMap.containsKey(currSorted)) {
-                hashMap.get(currSorted).add(strs[i]);
-            } else {
-                List<String> currVals = new ArrayList<>();
-                currVals.add(strs[i]);
-                hashMap.put(currSorted, currVals);
-            }
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] strArray = str.toCharArray();
+            Arrays.sort(strArray);
+            String strNew = new String(strArray);
+            map.putIfAbsent(strNew, new ArrayList<String>());
+            map.get(strNew).add(str);
         }
-        return new ArrayList<>(hashMap.values());
+
+        return new ArrayList<>(map.values());
+
     }
 }
