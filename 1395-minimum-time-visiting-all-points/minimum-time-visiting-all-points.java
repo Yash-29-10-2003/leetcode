45be9102b045761c81a14f5b39b134b80323d2ed
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2025-10-13 21:25:22 +0530

    Time: 1 ms (91.16%) | Memory: 43.1 MB (53.27%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -1,17 +1,10 @@
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
         int count = 0;
-        int[] cursor = Arrays.copyOf(points[0], 2);
         for (int i = 1; i < points.length; i++) {
-            while (cursor[0] != points[i][0] || cursor[1] != points[i][1]) {
-                if (cursor[0] < points[i][0]) cursor[0]++;
-                else if (cursor[0] > points[i][0]) cursor[0]--;
-
-                if (cursor[1] < points[i][1]) cursor[1]++;
-                else if (cursor[1] > points[i][1]) cursor[1]--;
-
-                count++;
-            }
+            int dx = Math.abs(points[i][0] - points[i-1][0]);
+            int dy = Math.abs(points[i][1] - points[i-1][1]);
+            count += Math.max(dx, dy);
         }
         return count;
     }
