dfe70d5b655df9b28724b5896f1324c3ea3246e3
Yash Singh <89728102+Yash-29-10-2003@users.noreply.github.com>
2026-04-25 06:20:41 +0530

    Time: 2 ms (95.14%) | Memory: 72.2 MB (17.41%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,32 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //we can think of the resultant array as for each array[i]
-        //array[i] = Mult of right of Array[i] * Mult of left of Array[i]
-
-        int[] resArray = new int[nums.length];
-        Arrays.fill(resArray, 1);
-
-        //multiplying every ele of the result array with ele's on its left side
-        int leftMult = 1;
-        for (int i = 0 ; i < nums.length ; i++){
-            if (i == 0){
-                resArray[i] = 1;
-            } else {
-                leftMult *= nums[i-1];
-                resArray[i] *= leftMult;
-            }
+        // [1,2,3,4,5]
+        // [120, 60, 40, 30, 24]
+        int[] res = new int[nums.length];
+        int product = 1;
+        //suffix array prep
+        for (int i = 0; i < nums.length ; i ++){
+            res[i] = product;
+            product*=nums[i];
         }
 
-        //multiplying every ele of the result array with ele's on its right side
-        int rightMult = 1;
-        for (int i = nums.length -1 ; i > -1 ; i--){
-            if (i == nums.length -1){
-                resArray[i] *= 1;
-            } else {
-                rightMult *= nums[i+1];
-                resArray[i] *= rightMult;
-            }
+        //multiplying with all prefixes now
+        product = 1;
+        for(int j = nums.length -1 ; j>= 0 ; j--){
+            res[j]*= product;
+            product *= nums[j];
         }
-        return resArray;
+        return res;
     }
 }
